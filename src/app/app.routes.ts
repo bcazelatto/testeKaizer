d1c0e904b45181884f0a56c2e816b750b3a1dcbf
Bruno Cazelatto <bcazelatto@gmail.com>
2021-02-24 15:19:00 -0300

    projeto alterado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import {Routes} from '@angular/router'
 
 import {HomeComponent} from './home/home.component'
-//import {AboutComponent} from './about/about.component'
+import {CadastrarClienteComponent} from './cadastrar-cliente/cadastrar-cliente.component'
 
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent}
-    //{path:'about', component: AboutComponent}
+    {path: '', component: HomeComponent},
+    {path: 'cadastrar-cliente', component: CadastrarClienteComponent}
 ]
